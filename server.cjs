a5d6830a313cd1a9820eef8ecf47a21b8486eca6
atharv-yoti <atharv.sinha@yoti.com>
2023-01-05 19:24:48 +0530

    dynamic qr

---
+++ server.cjs
@@ -0,0 +1,20 @@
+// const shareUrl = import("./index.js");
+const https = require("https");
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const pkg = require("body-parser");
+const { urlencoded, json } = pkg;
+
+app.use(urlencoded({ extended: false }));
+app.use(json());
+
+app.get("/", function (req, res) {
+  res.sendFile("/Users/sinhaath/Desktop/dynamicqr/index.html");
+});
+
+const options = {
+  key: fs.readFileSync("server.key"),
+  cert: fs.readFileSync("server.cert"),
+};
+https.createServer(options, app).listen(3000);
